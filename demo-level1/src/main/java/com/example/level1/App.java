3f566456192fd39866b08b545d0ab975e1ed73fe
Zohair Elzakzouk <z.elzakzouk@gmail.com>
2026-07-06 17:26:22 +0200

    Added new 1 & 2 comments.

--- demo-level1/src/main/java/com/example/level1/App.java
+++ demo-level1/src/main/java/com/example/level1/App.java
@@ -4,11 +4,12 @@ package com.example.level1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // print text
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // print text-1
+        System.out.println("Hello World! - 1");
+
+        // print text-2
+        System.out.println("Hello World! - 2");
     }
 }
